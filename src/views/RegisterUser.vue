9fdd4f88d5a6a5751a747e00bcf02d3c0a01b48d
mr-fool <mr-fool@users.noreply.github.com>
2020-04-17 10:52:43 -0600

    registration

--- src/views/RegisterUser.vue
+++ src/views/RegisterUser.vue
@@ -39,6 +39,9 @@ export default {
         email: this.email,
         password: this.password
       })
+      .then( () => {
+          this.$router.push({name: 'dashboard'})
+      })
     }
   }
 }
