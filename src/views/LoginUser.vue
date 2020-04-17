8328a021a0004fb6cd85c84e5fdcb68ebd5f6ac9
mr-fool <mr-fool@users.noreply.github.com>
2020-04-17 12:32:25 -0600

    Create LoginUser.vue

---
+++ src/views/LoginUser.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <form @submit.prevent="login">
+      <label for="email">
+        Email:
+      </label>
+      <input v-model="email" type="email" name="email" value />
+
+      <label for="password">
+        Password:
+      </label>
+      <input v-model="password" type="password" name="password" value />
+
+      <button type="submit" name="button">
+        Login
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
