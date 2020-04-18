7837998fd7ab0663e7e707952f2cf22f4e515067
mr-fool <mr-fool@users.noreply.github.com>
2020-04-17 23:59:41 -0600

    login

--- src/router.js
+++ src/router.js
@@ -19,7 +19,8 @@ const router = new Router({
     {
       path: '/dashboard',
       name: 'dashboard',
-      component: Dashboard
+      component: Dashboard,
+      meta: {requireAuth: true}
     },
     {
       path: '/register',
@@ -33,5 +34,11 @@ const router = new Router({
     }
   ]
 })
-
+router.beforeEach((to, from, next) => {
+  const loggedIn = localStorage.getItem('user')
+  if (to.matched.some(record => record.meta.requiresAuth) && !loggedIn) {
+    next('/')
+  }
+  next()
+})
 export default router
